3bb63ba692b49072c8f79dea8516b8f4c75910da
longpt271 <longpt271@gmail.com>
2023-12-02 21:57:31 +0700

    create list nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,32 @@ import { images } from "../constants";
 const Header = () => {
   return (
     <section>
-      <header>
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img src={images.Logo} alt="logo" />
         </div>
-        <div></div>
+        <div className="flex gap-x-9 items-center">
+          <ul className="flex gap-x-5 font-semibold">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Articles</a>
+            </li>
+            <li>
+              <a href="/">Pages</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+            <li>
+              <a href="/">Faq</a>
+            </li>
+          </ul>
+          <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+            Sign in
+          </button>
+        </div>
       </header>
     </section>
   );
