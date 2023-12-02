42170ddf771c52c3dd8cb8aaa16d867459d7531b
longpt271 <longpt271@gmail.com>
2023-12-02 22:04:56 +0700

    animation hover homepage li tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,13 @@ const Header = () => {
         </div>
         <div className="flex gap-x-9 items-center">
           <ul className="flex gap-x-5 font-semibold">
-            <li>
-              <a href="/">Home</a>
+            <li className="relative group">
+              <a href="/" className="px-4 py-2 ">
+                Home
+              </a>
+              <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+                /
+              </span>
             </li>
             <li>
               <a href="/">Articles</a>
