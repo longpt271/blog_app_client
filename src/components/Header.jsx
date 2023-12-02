57630a994896ce39f4f436248fe61d94eb56e307
longpt271 <longpt271@gmail.com>
2023-12-02 23:12:40 +0700

    css nav page dropdown icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import { images } from "../constants";
 const navItemsInfo = [
   { name: "Home", type: "link" },
   { name: "Articles", type: "link" },
-  { name: "Pages", type: "dropdown", items: ["About us, Contact"] },
+  { name: "Pages", type: "dropdown", items: ["About us", "Contact"] },
   { name: "Pricing", type: "link" },
   { name: "Faq", type: "link" },
 ];
@@ -25,21 +25,20 @@ const NavItem = ({ item }) => {
         </>
       ) : (
         <>
-          <a href="/" className="px-4 py-2 ">
-            {item.name}
+          <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+            <span>{item.name}</span>
+            <MdKeyboardArrowDown />
           </a>
           <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
             <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
               {item.items.map((page) => {
                 return (
-                  <li>
-                    <a
-                      href="/"
-                      className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
-                    >
-                      {page}
-                    </a>
-                  </li>
+                  <a
+                    href="/"
+                    className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                  >
+                    {page}
+                  </a>
                 );
               })}
             </ul>
