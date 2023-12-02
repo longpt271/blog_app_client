a05f6fb620461e19f036a12efbdb6af5c57a118a
longpt271 <longpt271@gmail.com>
2023-12-02 21:11:07 +0700

    test basic css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-5xl">Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;
