7d855b293b9c9ca27ea77c814dd11884b4021afe
longpt271 <longpt271@gmail.com>
2023-12-02 21:41:46 +0700

    const logo image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { images } from "../constants";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section>
+      <header>
+        <div>
+          <img src={images.Logo} alt="logo" />
+        </div>
+        <div></div>
+      </header>
+    </section>
+  );
 };
 
 export default Header;
