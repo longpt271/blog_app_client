97eafb8975d2d0ba40d59b3004cb618ddb68a1fe
longpt271 <longpt271@gmail.com>
2023-12-06 00:24:49 +0700

    hero input search

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
+
 import { images } from "../../../constants";
 
 const Hero = () => {
@@ -12,11 +14,18 @@ const Hero = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua
         </p>
-        <div>
-          <div>
-            <input type="text" />
+        <div className="flex flex-col gap-y-2.5 mt-10 relative">
+          <div className="relative">
+            <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]" />
+            <input
+              className="placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] md:py-4"
+              type="text"
+              placeholder="Search article"
+            />
           </div>
-          <button>Search</button>
+          <button className="w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2">
+            Search
+          </button>
         </div>
         <div>
           <span>Popular Tags:</span>
