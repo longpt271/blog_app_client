87db3e1c5dc11e35a7d32b1218b3bbfed8427c63
longpt271 <longpt271@gmail.com>
2023-12-07 02:50:01 +0700

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ import { images } from "../constants";
 const Footer = () => {
   return (
     <section className="bg-dark-hard">
-      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5">
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">Product</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">Landingpage</a>
             </li>
@@ -33,9 +33,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">Services</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">Services</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">Documentation</a>
             </li>
@@ -53,9 +53,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">Company</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+        <div className="col-span-5 md:col-span-4 md:col-start-5 lg:col-start-auto lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">Company</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">About</a>
             </li>
@@ -70,9 +70,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold">More</h3>
-          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">More</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
               <a href="/">Documentation</a>
             </li>
@@ -84,16 +84,16 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-10">
+        <div className="col-span-10 md:order-first md:col-span-4 lg:col-span-2">
           <img
             src={images.Logo}
             alt="logo"
-            className="brightness-0 invert mx-auto"
+            className="brightness-0 invert mx-auto md:mx-0"
           />
-          <p className="text-sm text-dark-light text-center mt-4">
+          <p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base lg:text-sm">
             Build a modern and creative website with crealand
           </p>
-          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300">
+          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
             <li>
               <a href="/">
                 <AiOutlineTwitter className="w-6 h-auto" />
