6dd492a4b8e2a47e59e9cb768bac1b9172b33378
longpt271 <longpt271@gmail.com>
2023-12-07 02:36:10 +0700

    Footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,83 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="bg-dark-hard">
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5">
+        <div className="col-span-5">
+          <h3 className="text-dark-light font-bold">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+            <li>
+              <a href="/">Landingpage</a>
+            </li>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">Documentation</a>
+            </li>
+            <li>
+              <a href="/">Referral Program</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-5">
+          <h3 className="text-dark-light font-bold">Services</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+            <li>
+              <a href="/">Documentation</a>
+            </li>
+            <li>
+              <a href="/">Design</a>
+            </li>
+            <li>
+              <a href="/">Themes</a>
+            </li>
+            <li>
+              <a href="/">Illustrations</a>
+            </li>
+            <li>
+              <a href="/">UI Kit</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-5">
+          <h3 className="text-dark-light font-bold">Company</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Terms</a>
+            </li>
+            <li>
+              <a href="/">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/">Careers</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-5">
+          <h3 className="text-dark-light font-bold">More</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
+            <li>
+              <a href="/">Documentation</a>
+            </li>
+            <li>
+              <a href="/">License</a>
+            </li>
+            <li>
+              <a href="/">Changelog</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </section>
+  );
 };
 
 export default Footer;
