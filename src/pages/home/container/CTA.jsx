a2a69d9f207a7abcbcf1f36009f9369c457d1ec2
longpt271 <longpt271@gmail.com>
2023-12-06 23:17:05 +0700

    CTA form

--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <>
       <svg
-        className="w-full h-auto max-h-40"
+        className="w-full h-auto max-h-40 translate-y-[1px]"
         preserveAspectRatio="none"
         viewBox="0 0 2160 263"
         fill="none"
@@ -19,7 +19,32 @@ const CTA = () => {
         />
       </svg>
 
-      <section></section>
+      <section className="relative bg-dark-hard px-5">
+        <div className="container grid grid-cols-12 mx-auto py-10">
+          <div className="col-span-12">
+            <h2 className="text-white font-roboto font-bold text-2xl">
+              Get our stories delivered From us to your inbox weekly.
+            </h2>
+            <div className="w-full max-w-[494px] mt-12 space-y-3 mx-auto">
+              <input
+                type="text"
+                className="px-4 py-3 rounded-lg w-full placeholder:text-dark-light"
+                placeholder="Your Email"
+              />
+              <button className="px-4 py-3 rounded-lg w-full bg-primary text-white font-bold">
+                Get started
+              </button>
+            </div>
+            <p className="text-dark-light text-sm leading-7 mt-6">
+              <span className="font-bold italic text-[#B3BAC5]">
+                Get a response tomorrow
+              </span>{" "}
+              if you submit by 9pm today. If we received after 9pm will get a
+              reponse the following day.
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
