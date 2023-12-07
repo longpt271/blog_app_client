c572d35b56a0e38a9a3b7417e4b934445d92e846
longpt271 <longpt271@gmail.com>
2023-12-07 23:35:17 +0700

    ArticleDetailPage route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/home/HomePage";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 
 function App() {
   return (
     <div className="App font-opensans">
       <Routes>
         <Route index path="/" element={<HomePage />} />
+        <Route index path="/blog/:id" element={<ArticleDetailPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainLayout from "../../components/MainLayout";
+
+const ArticleDetailPage = () => {
+  return (
+    <MainLayout>
+      <section>
+        <h1>Article Detail Page</h1>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default ArticleDetailPage;
