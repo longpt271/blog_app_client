7459c4007a1a8ee0a2c8eb4a746ea438f36f9435
longpt271 <longpt271@gmail.com>
2023-12-07 23:30:30 +0700

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
     <div className="App font-opensans">
-      <HomePage />
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
