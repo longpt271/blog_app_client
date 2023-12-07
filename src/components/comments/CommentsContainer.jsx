707fe58c28a1920b6a73435f0d0d9c7b3d45fd53
longpt271 <longpt271@gmail.com>
2023-12-08 02:34:20 +0700

    replied Comment form

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -7,6 +7,7 @@ import Comment from "./Comment";
 const CommentsContainer = ({ className, logginedUserId }) => {
   const [comments, setComments] = useState([]);
   const mainComments = comments.filter((comment) => comment.parent === null);
+  const [affectedComment, setAffectedComment] = useState(null);
 
   console.log(comments);
 
@@ -44,7 +45,13 @@ const CommentsContainer = ({ className, logginedUserId }) => {
       />
       <div className="space-y-4 mt-8">
         {mainComments.map((comment) => (
-          <Comment comment={comment} logginedUserId={logginedUserId} />
+          <Comment
+            comment={comment}
+            logginedUserId={logginedUserId}
+            affectedComment={affectedComment}
+            setAffectedComment={setAffectedComment}
+            addComment={addCommentHandler}
+          />
         ))}
       </div>
     </div>
