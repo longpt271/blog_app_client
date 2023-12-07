d349681d33dc8becebe93cf57a69005acc7c2d43
longpt271 <longpt271@gmail.com>
2023-12-07 23:50:59 +0700

    BreadCrumbs links

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import MainLayout from "../../components/MainLayout";
+import BreadCrumbs from "../../components/BreadCrumbs";
+
+const breadCrumbsData = [
+  { name: "Home", link: "/" },
+  { name: "Blog", link: "/blog" },
+  { name: "Article title", link: "/blog/1" },
+];
 
 const ArticleDetailPage = () => {
   return (
     <MainLayout>
-      <section>
-        <h1>Article Detail Page</h1>
+      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5">
+        <article className="flex-1">
+          <BreadCrumbs data={breadCrumbsData} />
+        </article>
       </section>
     </MainLayout>
   );
