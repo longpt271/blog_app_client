5933eaebaa84b8fc4e447841384a2952480eb41b
longpt271 <longpt271@gmail.com>
2023-12-08 00:45:22 +0700

    SuggestedPosts responsive

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -51,7 +51,7 @@ const tagsData = [
 const ArticleDetailPage = () => {
   return (
     <MainLayout>
-      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5">
+      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
         <article className="flex-1">
           <BreadCrumbs data={breadCrumbsData} />
           <img
@@ -84,7 +84,7 @@ const ArticleDetailPage = () => {
           header="Latest Article"
           posts={postsData}
           tags={tagsData}
-          className="mt-8"
+          className="mt-8 lg:mt-0 lg:max-w-xs"
         />
       </section>
     </MainLayout>
--- src/pages/articleDetail/container/SuggestedPosts.jsx
+++ src/pages/articleDetail/container/SuggestedPosts.jsx
@@ -6,8 +6,10 @@ const SuggestedPosts = ({ className, header, posts = [], tags }) => {
     <div
       className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 ${className}`}
     >
-      <h2 className="font-roboto font-medium text-dark-hard">{header}</h2>
-      <div className="grid gap-y-5 mt-5">
+      <h2 className="font-roboto font-medium text-dark-hard md:text-xl">
+        {header}
+      </h2>
+      <div className="grid gap-y-5 mt-5 md:grid-cols-2 md:gap-x-5 lg:grid-cols-1">
         {posts.map((item) => (
           <div
             key={item._id}
@@ -19,7 +21,7 @@ const SuggestedPosts = ({ className, header, posts = [], tags }) => {
               alt="laptop"
             />
             <div className="text-sm font-roboto text-dark-hard font-medium">
-              <h3 className="text-sm font-roboto text-dark-hard font-medium">
+              <h3 className="text-sm font-roboto text-dark-hard font-medium md:text-base lg:text-lg">
                 {item.title}
               </h3>
               <span className="text-xs opacity-60">
@@ -33,13 +35,15 @@ const SuggestedPosts = ({ className, header, posts = [], tags }) => {
           </div>
         ))}
       </div>
-      <h2 className="font-roboto font-medium text-dark-hard mt-8">Tags</h2>
+      <h2 className="font-roboto font-medium text-dark-hard mt-8 md:text-xl">
+        Tags
+      </h2>
       <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
         {tags.map((item, index) => (
           <Link
             key={index}
             to="/"
-            className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white"
+            className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm"
           >
             {item}
           </Link>
