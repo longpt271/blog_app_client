dfdae941fc085e55ea10769a5ecbc5e288ae7e59
longpt271 <longpt271@gmail.com>
2023-12-12 18:53:15 +0700

    handleCropImage

--- src/components/crop/CropEasy.jsx
+++ src/components/crop/CropEasy.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import Cropper from "react-easy-crop";
 
-const CropEasy = ({ photo }) => {
+import getCroppedImg from "./cropImage";
+
+const CropEasy = ({ photo, setOpenCrop }) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setzoom] = useState(1);
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
 
   const handleCropComplete = (cropedArea, cropedAreaPixels) => {
     setCroppedAreaPixels(cropedAreaPixels);
+  };
+
+  const handleCropImage = async () => {
+    try {
+      const croppedImg = await getCroppedImg(photo?.url, croppedAreaPixels);
+
+      const file = new File([croppedImg.file], `${photo?.file?.name}`, {
+        type: photo?.file?.type,
+      });
+
+      const formData = new FormData();
+      formData.append("profilePicture", file);
+    } catch (error) {}
   };
   return (
     <div className="fixed z-[1000] inset-0 bg-black/50 flex justify-center p-5 overflow-auto">
@@ -23,6 +38,38 @@ const CropEasy = ({ photo }) => {
             onCropChange={setCrop}
             onCropComplete={handleCropComplete}
           />
+        </div>
+        <div>
+          <label
+            htmlFor="zoomRage"
+            className="block mt-2 mb-0.5 text-sm font-medium text-gray-900"
+          >
+            Zoom: {`${Math.round(zoom * 100)}%`}
+          </label>
+          <input
+            type="range"
+            id="zoomRange"
+            min={1}
+            max={3}
+            step={0.1}
+            value={zoom}
+            onChange={(e) => setzoom(e.target.value)}
+            className="w-full h-1 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm"
+          />
+        </div>
+        <div className="flex justify-between gap-2 flex-wrap">
+          <button
+            onClick={() => setOpenCrop(false)}
+            className="px-5 py-2.5 rounded-lg text-red-500 border border-red-500 text-sm disabled:opacity-70"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={handleCropImage}
+            className="px-5 py-2.5 rounded-lg text-white bg-blue-500 text-sm disabled:opacity-70"
+          >
+            Crop & Upload
+          </button>
         </div>
       </div>
     </div>
