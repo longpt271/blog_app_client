5db7616c83bd07680820fbb000237c848e1d1bbe
longpt271 <longpt271@gmail.com>
2023-12-18 01:22:46 +0700

    toggleMenuHandler

--- src/pages/admin/components/header/Header.jsx
+++ src/pages/admin/components/header/Header.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+import { images } from "../../../../constants";
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const toggleMenuHandler = () => {
+    setIsMenuActive((prevState) => !prevState);
+  };
+
+  return (
+    <header className="flex h-fit w-full items-center justify-between p-4">
+      {/* logo */}
+      <Link to="/">
+        <img src={images.Logo} alt="logo" className="w-16" />
+      </Link>
+      {/* menu burger icon */}
+      <div className="cursor-pointer">
+        {isMenuActive ? (
+          <AiOutlineClose className="w-6 h-6" onClick={toggleMenuHandler} />
+        ) : (
+          <AiOutlineMenu className="w-6 h-6" onClick={toggleMenuHandler} />
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
