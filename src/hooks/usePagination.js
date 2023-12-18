2d479445e967c5dffa6ac9b303a55ded3b4bd3f5
longpt271 <longpt271@gmail.com>
2023-12-18 17:43:17 +0700

    Pagination range

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,5 +1,25 @@
+import { useMemo } from "react";
+
 export const usePagination = ({
   siblingCount = 1,
   currentPage,
   totalPageCount,
-}) => {};
+}) => {
+  const paginationRange = useMemo(() => {
+    // our core logic goes here
+    const totalPageNumbers = siblingCount + 5;
+
+    // State 1: if the number of pages is less than the page numbers
+    if (totalPageNumbers >= totalPageCount) {
+      return range(1, totalPageCount);
+    }
+  }, [siblingCount, currentPage, totalPageCount]);
+
+  return paginationRange;
+};
+
+function range(start, end) {
+  const length = end - start + 1;
+
+  return Array.from({ length }, (value, index) => index + start);
+}
