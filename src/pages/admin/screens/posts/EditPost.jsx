953ccb2c7cea6cfa0bf2df169f437915c89c8c5d
longpt271 <longpt271@gmail.com>
2023-12-18 18:56:19 +0700

    copy jsx from ArticleDetailPage

--- src/pages/admin/screens/posts/EditPost.jsx
+++ src/pages/admin/screens/posts/EditPost.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { getSinglePost } from "../../../../services/index/posts";
+import { useParams } from "react-router-dom";
+import ArticleDetailSkeleton from "../../../articleDetail/components/ArticleDetailSkeleton";
+import ErrorMessage from "../../../../components/ErrorMessage";
 
 const EditPost = () => {
-  return <div>EditPost</div>;
+  const { slug } = useParams();
+  const [initialPhoto, setInitialPhoto] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [body, setBody] = useState(null);
+
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getSinglePost({ slug }),
+    queryKey: ["blog", slug],
+  });
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      setInitialPhoto(data?.photo);
+      setBody();
+    }
+  }, [data, isError, isLoading]);
+  return (
+    <div>
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Couldn't fetch the post detail" />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/blog?category=${category.name}`}
+                  className="text-primary text-sm font-roboto inline-block md:text-base"
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+            <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard">
+              {data?.title}
+            </h1>
+            <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+            <CommentsContainer
+              comments={data?.comments}
+              className="mt-10"
+              logginedUserId={userState?.userInfo?._id}
+              postSlug={slug}
+            />
+          </article>
+          <div>
+            <SuggestedPosts
+              header="Latest Article"
+              posts={postsData?.data}
+              tags={data?.tags}
+              className="mt-8 lg:mt-0 lg:max-w-xs"
+            />
+            <div className="mt-7">
+              <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+                Share on:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI(window.location.href)}
+                title={encodeURIComponent(data?.title)}
+              />
+            </div>
+          </div>
+        </section>
+      )}
+    </div>
+  );
 };
 
 export default EditPost;
